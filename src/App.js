bdc319e133f603930bd2e739e357aa72d8b6e7cc
Chris In <christopherin12@gmail.com>
2022-03-03 20:33:51 -0500

    posts component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
-
+import Post from './Post';
 function App() {
+  const [posts, setPosts] = useState([
+    {
+      username: "topher",
+      caption: "ayy",
+      imageUrl: "https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+    },
+    {
+      username: "bubba",
+      caption: "dink",
+      imageUrl: "https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+    }
+  ]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className='app__header'>
+          <img
+            className='app__headerImage'
+            alt=""
+            src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+          />
+        </div>
+        <h1>YuHHH</h1>
+
+        {/* Posts */}
+        {
+          posts.map(post => (
+            <Post username={post.username} caption={post.caption} imageUrl={post.imageUrl}/>
+          ))
+        }
+
     </div>
   );
 }
---
+++ src/Post.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import "./Post.css"
+import {Avatar} from "@mui/material"
+function Post({ username, caption, imageUrl }) {
+  return (
+    <div className="post">
+        {/* header-> avatar + username */}
+        <div className='post__header'>
+            <Avatar
+                className="post__avatar"
+                alt="topher.in"
+                src="static/images/avatar/1.jpg"
+            />
+            <h3>{username}</h3>
+        </div>
+
+        <img className="post__image" src= {imageUrl} alt=""></img>
+        <h4 className='post__text'><strong>{username}</strong> {caption}</h4>
+    </div>
+  )
+}
+
+export default Post
